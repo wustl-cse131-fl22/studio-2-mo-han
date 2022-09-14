3b1b5c3a51dcada058b3c2d3a0df196093fb8b41
moyusii <moyusii@localhost>
2022-09-14 16:10:13 -0500

    hi michelle

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -1,11 +1,16 @@
 package studio2;
 
+import java.util.Scanner;
+
 public class Ruin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner in 
+		Scanner in = new Scanner (System.in);
+		System.out.println("What is the start amount?");
+		int startAmount = in.nextInt();
+		System.out.println("What is ")
 	}
 
 }
